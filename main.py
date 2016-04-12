67ee9ce242ec7d8fbbcb30baec5647caa47c9cf5
Udit <codenirvana28@gmail.com>
2016-04-12 20:18:32 +0530

    send sms using twilio

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+#!/usr/bin/python
 import httplib2
+import json
 import os
 
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
+from twilio.rest import TwilioRestClient
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail-SMS'
 
-def get_credentials():
+def GetCredentials():
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -32,22 +35,52 @@ def ListMessages(service):
     if 'messages' in response:
         messages.extend(response['messages'])
 
-    return [message['id'] for message in messages]
+    messages = [message['id'] for message in messages]
+    return messages
 
 def GetMessage(service, msg_id):
     message = service.users().messages().get(userId="me", id=msg_id, format='metadata',metadataHeaders='Subject').execute()
     return message
 
+def WorkingDirectory():
+    cwd = os.getcwd()
+    if 'gmail-sms-notify' in cwd:
+        cwd = ''
+    else:
+        cwd = os.path.expanduser('~') + '/gmail-sms-notify/'
+
+    return cwd
+
 def GetLogs():
-    return [line.rstrip('\n') for line in open('logs')]
+    lines = open( WorkingDirectory() + 'logs','r' )
+    logs = [line.rstrip('\n') for line in lines]
+    lines.close()
+    return logs
 
 def UpdateLogs(messages):
-    logs = open('logs','w')
+    logs = open( WorkingDirectory() + 'logs','w' )
     logs.write('\n'.join(messages))
     logs.close()
 
+def GetTwilio():
+    f = open( WorkingDirectory() + 'twilio.json', 'r' )
+    twilio = json.load(f)
+    f.close()
+    return twilio
+
+def SendSMS(msgSubject):
+    twilio = GetTwilio()
+
+    client = TwilioRestClient( twilio['sid'], twilio['auth_token'] )
+
+    message = client.messages.create(
+        to = twilio["number_to"],
+        from_ = twilio["number_from"],
+        body = msgSubject
+    )
+
 def main():
-    credentials = get_credentials()
+    credentials = GetCredentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
@@ -57,7 +90,8 @@ def main():
     for msg in msgList:
         if msg not in prevMessages:
             msg = GetMessage(service, msg)
-            print(msg['payload']['headers'][0]['value'])
+            msgSubject = msg['payload']['headers'][0]['value']
+            SendSMS(msgSubject)
 
     UpdateLogs(msgList)
 
